dd27c6cd8db91d0fccfcf3ca0570c004d2713d80
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-04-21 11:37:42 +0530

    adding prisma and writing some intial schema

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -2,3 +2,16 @@ import express from 'express';
 
 const app = express();
 const port = 3000;  
+
+
+// password protection middleware
+
+app.post("/hooks/catch:userId/:zapId", (req, res) => {  
+  const { userId, zapId } = req.params;
+  const { body } = req;
+  console.log(`Received webhook for userId: ${userId}, zapId: ${zapId}`);
+  console.log("Request body:", body);   
+  // start in db a new trigger 
+  // push it to a queue (kaafka, redis, etc.)
+    // trigger the zap
+} )
