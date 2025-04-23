070c6e10423a0e4f57f2b9d1cc5139de53fa0f8e
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-04-23 11:15:31 +0530

    adding kafka to process the async backend calls

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,10 +1,20 @@
 
+import { Kafka } from "kafkajs";
 import { PrismaClient } from "./generated/prisma";
+
 
 
 const client = new PrismaClient();
 
+// Kafka client configuration
+const kafka = new Kafka({
+    clientId: "outbox-processor", 
+    brokers: ["localhost:9092"],    
+})
+
 async function main() {
+    const producer = kafka.producer();
+    await producer.connect();
     while (true) {
         const pendingRows = await client.zapRunOutbox.findMany({
             where:{},
