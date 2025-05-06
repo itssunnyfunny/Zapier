ffae7056b6a235997dd715f0896807d2388dbe78
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-06 09:48:37 +0530

    adding primary-backend to the main forlder

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,14 +1,15 @@
 import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
-    clientId: "outbox-processor",    
-    brokers: ["localhost:9092"], 
+    clientId: "outbox-processor",     
+    brokers: ["localhost:9092"],  
 });
 
 const TOPIC_NAME = "zap-event"; 
 async function main() {
     const consumer = kafka.consumer({ groupId: "main-worker" });
     await consumer.connect();
+    console.log("Connected to Kafka consumer");
     await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true }); 
     await consumer.run({
         autoCommit:false,
@@ -21,11 +22,11 @@ async function main() {
             await consumer.commitOffsets([{
                 topic: TOPIC_NAME,
                 partition: partition,
-                offset: message.offset,
+                offset: (parseInt(message.offset) + 1).toString(), // Commit the offset of the next message to be processed
             }
-      
+          
         ]);
-     
+        
         },
     });
 }
