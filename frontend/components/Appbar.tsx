5f9cdad1283bda45372cc8ea1862f8e788ed3702
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-07 12:42:01 +0530

    adding hero section of the landing page

--- frontend/components/Appbar.tsx
+++ frontend/components/Appbar.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import LinkButton from './buttons/LinkButton'
 import { useRouter } from 'next/navigation'
+import PrimaryButton from './buttons/PrimaryButton';
 
 const Appbar = () => {
     const router = useRouter();
   return (
     <div>
-        <div>Zapier</div>
-        <div>
+        <div className='flex flex-col justify-center'>Zapier</div>
+        <div className='flex items-center justify-center gap-4'>
             <LinkButton onclick={()=>{}}>Contact Sales</LinkButton>
             <LinkButton onclick={()=>{
                 router.push('/login')
             }}>Log In</LinkButton>
+            <PrimaryButton  onClick={()=>{
+                router.push('/signup')
+            }
+            }>Sign Up</PrimaryButton>
         </div>
     </div>
   )
--- frontend/components/buttons/PrimaryButton.tsx
+++ frontend/components/buttons/PrimaryButton.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const PrimaryButton = ({children, onClick, size = 'small'}:{
     children: React.ReactNode, 
     onClick: () => void, 
-    size: 'small' | 'large'
+    size?: 'small' | 'large'
 }) => {
-    return <div onClick={onClick} className={`${size === "small" ? "text-sm" : "text-xl"} ${size === "small" ? "px-8 py-2" : "px-10 py-4"} cursor-pointer hover:shadow-md bg-amber-700 text-white rounded-full text-center flex justify-center flex-col`}>
+    return <div onClick={onClick} className={`${size === "small" ? "text-sm" : "text-xl"} ${size === "small" ? "px-8 py-2" : "px-10 py-4"} cursor-pointer hover:shadow-md bg-orange-700 text-white rounded-full text-center flex justify-center flex-col`}>
     {children}
 </div>
 }
