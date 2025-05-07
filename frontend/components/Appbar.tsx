f974c2295b761f3aa36ca17295a99377c7bf2489
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-07 11:49:51 +0530

    adding primary button

--- frontend/components/Appbar.tsx
+++ frontend/components/Appbar.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import LinkButton from './buttons/LinkButton'
+import { useRouter } from 'next/navigation'
 
 const Appbar = () => {
+    const router = useRouter();
   return (
     <div>
         <div>Zapier</div>
-        <div></div>
+        <div>
+            <LinkButton onclick={()=>{}}>Contact Sales</LinkButton>
+            <LinkButton onclick={()=>{
+                router.push('/login')
+            }}>Log In</LinkButton>
+        </div>
     </div>
   )
 }
---
+++ frontend/components/buttons/PrimaryButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PrimaryButton = ({children, onClick, size = 'small'}:{
+    children: React.ReactNode, 
+    onClick: () => void, 
+    size: 'small' | 'large'
+}) => {
+    return <div onClick={onClick} className={`${size === "small" ? "text-sm" : "text-xl"} ${size === "small" ? "px-8 py-2" : "px-10 py-4"} cursor-pointer hover:shadow-md bg-amber-700 text-white rounded-full text-center flex justify-center flex-col`}>
+    {children}
+</div>
+}
+
+export default PrimaryButton
