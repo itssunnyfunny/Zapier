86802765f7a2cc285fc211d000fce53e9048fe13
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-05-19 12:48:48 +0530

    adding some changes in different files

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+ import Appbar from "@/components/Appbar";
+
+ export default function() {
+
+       return <div>
+        <Appbar />
+
+
+        <div className="flex justify-center ">
+            <div className="flex-1 pt-20 px-4">
+                <div className="font-semibold text-3xl pb-4">
+                    Welcome back, 
+                </div>
+            </div>
+        </div>
+
+        </div>
+ }
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -18,7 +18,7 @@ export default function() {
 
     return <div> 
         <Appbar />
-        <div className="flex justify-center">
+        <div className="flex justify-center">          
             <div className="flex pt-8 max-w-4xl">
                 <div className="flex-1 pt-20 px-4">
                     <div className="font-semibold text-3xl pb-4">
@@ -33,7 +33,8 @@ export default function() {
                     <CheckFeature label={"14-day trial of premium features & apps"} />
 
                 </div>
-                <div className="flex-1 pt-6 pb-6 mt-12 px-4 border rounded">
+
+                 <div className="flex-1 pt-6 pb-6 mt-12 px-4  border rounded">
                     <Input label={"Name"} onChange={e => {
                         setName(e.target.value)
                     }} type="text" placeholder="Your name"></Input>
